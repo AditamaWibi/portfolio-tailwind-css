0e4d10467bd7b2ee2863c92522eaa1a2497fb60f
AditamaWibi <wibiaditama78@gmail.com>
2024-05-07 09:35:31 +0700

    update dark mode + perbaikan fitur

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const topBtn = document.querySelector("#top-btn");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    topBtn.classList.remove("hidden");
+    topBtn.classList.add("block");
   } else {
     header.classList.remove("navbar-fixed");
+    topBtn.classList.remove("block");
+    topBtn.classList.add("hidden");
   }
 };
 
@@ -18,3 +23,36 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Close Hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Change Dark Toggle Position based on Local Storage
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
